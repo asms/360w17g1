fab29e4d10daa8e46d9ecc3aca610ce0ef63f60d
Steven Smith <deans93@uw.edu>
2017-02-06 20:47:37 -0800

    Made constructors public so that they can be instantiated.

--- src/model/ParkManager.java
+++ src/model/ParkManager.java
@@ -7,7 +7,7 @@ public class ParkManager extends User {
 	 */
 	private static final long serialVersionUID = -5509981249651008018L;
 
-	protected ParkManager(String theUserName) {
+	public ParkManager(String theUserName) {
 		super(theUserName);
 		// TODO Auto-generated constructor stub
 	}

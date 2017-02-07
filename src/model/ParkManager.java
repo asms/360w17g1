2b691140f63e0e5514f53326131034f370f80452
Steven Smith <deans93@uw.edu>
2017-02-06 20:57:51 -0800

    Added getAssociatedParks() method to ParkManager.

--- src/model/ParkManager.java
+++ src/model/ParkManager.java
@@ -1,15 +1,37 @@
 package model;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ParkManager extends User {
 
 	/**
 	 * Generated serial version UID.
 	 */
 	private static final long serialVersionUID = -5509981249651008018L;
+	
+	/**
+	 * The parks that the park manager manages.
+	 */
+	private final Set<Park> myParks;
 
-	public ParkManager(String theUserName) {
+	/**
+	 * Instantiates the park manager. This should be created by the ParkController.
+	 * @param theUserName the park manager's username
+	 * @param theParks the parks that the park manager manages
+	 */
+	public ParkManager(final String theUserName, final HashSet<Park> theParks) {
 		super(theUserName);
-		// TODO Auto-generated constructor stub
+		myParks = theParks;
+	}
+	
+	/**
+	 * Gets an unmodifiable set of the parks associated with the park manager.
+	 * @return the set of parks
+	 */
+	public Set<Park> getAssociatedParks() {
+		return Collections.unmodifiableSet(myParks);
 	}
 
 }

26b0dc14988269baeb8a926f716356b064f8ee0f
Steven Smith <deans93@uw.edu>
2017-02-08 17:13:52 -0800

    Commented ParkManagerView.

--- src/ui/ParkManagerView.java
+++ src/ui/ParkManagerView.java
@@ -4,21 +4,23 @@
  */
 package ui;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 import model.Job;
-import model.JobController;
 import model.Park;
 import ui.Command.CommandExecutor;
 import users.User;
 
+/**
+ * View for park managers only
+ * .
+ * @author Steven Smith
+ * @version 1.0
+ */
 public class ParkManagerView extends AbstractView {
 	
 	/**
@@ -26,13 +28,24 @@ public class ParkManagerView extends AbstractView {
 	 */
 	private static final int MAX_FUTURE_DATE = 1;
 	
+	/**
+	 * Possible commands a park manager can execute.
+	 */
 	private static enum COMMAND implements Command {
 		CREATE_NEW_JOB("Create a new job"),
 		VIEW_JOBS("View jobs"),
+		VIEW_VOLUNTEERS("View jobs"),
 		LOGOUT("Logout");
 		
+		/**
+		 * The human readable string for the command.
+		 */
 		private final String myValue;
 		
+		/**
+		 * Assigns a human readable string to the command.
+		 * @param theValue the string to be displayed for the command
+		 */
 		COMMAND(final String theValue) {
 			myValue = theValue;
 		}
@@ -43,8 +56,16 @@ public class ParkManagerView extends AbstractView {
 		}
 	}
 	
+	/**
+	 * The list of all possible commands for a park manager.
+	 */
 	private final Map<Command, CommandExecutor> commands;
 	
+	/**
+	 * Instantiates the park manager view.
+	 * @param theScanner the scanner object
+	 * @param theUser the logged in user
+	 */
 	public ParkManagerView(final Scanner theScanner, final User theUser) {
 		super(theScanner, theUser);
 		commands = new HashMap<Command, CommandExecutor>();
@@ -58,6 +79,12 @@ public class ParkManagerView extends AbstractView {
 			@Override
 			public void execute() {
 				viewJobs();
+			}
+		});
+		commands.put(COMMAND.VIEW_VOLUNTEERS, new CommandExecutor() {
+			@Override
+			public void execute() {
+				viewVolunteers();
 			}
 		});
 		commands.put(COMMAND.LOGOUT, new CommandExecutor() {
@@ -80,6 +107,9 @@ public class ParkManagerView extends AbstractView {
 		}
 	}
 	
+	/**
+	 * User Story: As a Park Manager I want to submit a new job.
+	 */
 	private void createNewJob() {
 		displayTitle("Create a new job");
 		final String name = getString("Enter job title");
@@ -119,12 +149,34 @@ public class ParkManagerView extends AbstractView {
 		getString("Press enter to continue...");
 	}
 	
+	/**
+	 * Views all jobs. Not a user story, but useful for debugging job creation.
+	 */
 	private void viewJobs() {
 		displayTitle("All Jobs");
 		displayNumberedList(myJobController.getUpcomingJobs().toArray());
 		getString("Press enter to continue...");
 	}
 	
+	/**
+	 * User Story: As a Park Manager I want to view a numbered list of Volunteers for a job (past or present) in the
+	 * parks that I manage.
+	 */
+	private void viewVolunteers() {
+		/*
+		 * TODO:
+		 * 
+		 * ALTERNATIVELY, this can be a subcommand of the view jobs method.
+		 * 
+		 * Get all past and present jobs.
+		 * Get volunteers associated with those jobs.
+		 * Display volunteers in a numbered list.
+		 */
+	}
+	
+	/**
+	 * Exits the view.
+	 */
 	private void logout() {
 		exit();
 	}

631c31e6d12605cb13685150ef158f8775b171e4
foxirving <amy.eli.irving@gmail.com>
2017-02-09 12:23:27 -0800

    Added comments to abstract controller

--- src/tests/AbstractControllerTest.java
+++ src/tests/AbstractControllerTest.java
@@ -18,37 +18,49 @@ import model.Volunteer;
  * Tests the AbstractController class.
  * 
  * <p>
- * The primary use for the abstract controller class is to control serialization and deserialization.
+ * The primary use for the abstract controller class is to control serialization
+ * and deserialization.
  * </p>
+ * 
  * @author Steven Smith
  * @version 1.0
  *
  */
 public class AbstractControllerTest {
-	
+
+	/**
+	 * JobController objects used for testing.
+	 */
 	private UserController myController;
 
 	@Before
 	public void setUp() {
 		myController = new UserController();
 	}
-	
+
 	/*
 	 * Currently this does not pass because AbstractController is not complete.
 	 */
 	@Test
 	public void testSerialization() {
-		final String random = String.valueOf(Math.random());
+		// Creates a random Volunteer and adds it to my controller.
+		// myController then should serialize the data.
 		Volunteer testObject = new Volunteer("username");
 		myController.addUser(testObject);
-		
-		
+
+		// A new instance of UserController should deserialize the data and the
+		// list should contain the user we added above.
 		myController = new UserController();
 		ArrayList<User> objects = myController.getAllUsers();
 		for (User user : objects) {
 			System.out.println(user.getUserName());
 		}
 		assertTrue(objects.contains(testObject));
+	}
+	
+	@Test
+	public void testClear(){
+		
 	}
 
 }

a5c39691c9eaf7895f7eddd0e264acdf5c22b4f7
Bryce <ba2012@uw.edu>
2017-02-12 18:52:46 -0800

    Added more tests

--- src/tests/JobControllerTest.java
+++ src/tests/JobControllerTest.java
@@ -5,10 +5,12 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -51,7 +53,7 @@ public class JobControllerTest
     public void testGetUpcomingJobs()
     {
         final ArrayList<Job> jobs = new ArrayList<Job>();
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 29; i++)
         {
         	final Job job = new Job("Trail Clearing" + i, new Park("BirchCreek Park", "address"), new Date(),  new Date(),  new Date(), 
                     "This job involves a lot of walking", i, 2, 4);
@@ -87,5 +89,53 @@ public class JobControllerTest
     }
 
 
+	@Test
+	public void testGetAllJobs_NoJobs() {
+		//JobController myJobController1 = new JobController();
+		
+		assertTrue(myJobController.getAllJobs().isEmpty());
+		
+	}
+    
+	@Test
+	public void testCanAddWithDate() {
+		
+		Date date = new Date(3/14/2017);
+		final Job firstJob = new Job("Trail Clearing", new Park("BirchCreek Park", "address"), date, new Date(), new Date(), 
+                "This job involves a lot of walking", 1, 2, 4);
 
+		final Job anotherJob =  new Job("Rail Clearing", new Park("BirchCreek Park", "address"), date, new Date(), new Date(), 
+                "This job involves a lot of walking", 1, 2, 4);
+		
+		final Job a3rdJob =  new Job("Ail Clearing", new Park("BirchCreek Park", "address"), date, new Date(), new Date(), 
+                "This job involves a lot of walking", 1, 2, 4);
+		
+		
+		myJobController.addJob(firstJob);
+		assertTrue(myJobController.canAddWithDate(date));
+		myJobController.addJob(anotherJob);
+		assertTrue(myJobController.canAddWithDate(date));
+		myJobController.addJob(a3rdJob);
+		assertFalse(myJobController.canAddWithDate(date));
+	}
+	
+	@Test
+	public void testCanAddWithNameAtPark() {
+		
+		
+	}
+	
+	@Test
+	public void testGetMaximumNumberOfPendingJobs_WithMax() {
+
+		assertTrue(myJobController.getMaximumNumberOfPendingJobs() == 30);
+	}
+    
+	
+	@Test
+	public void testGetMaximumNumberOfPendingJobs_NotMax() {
+
+		assertFalse(myJobController.getMaximumNumberOfPendingJobs() == 15);
+	}
+    
 }

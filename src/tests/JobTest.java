5ef13be3ef1a570190fe385b8d0449a37fcd8d91
ntrinh139 <1309.nguyentrinh@gmail.com>
2017-02-13 17:35:11 -0800

    small adjust

--- src/tests/JobTest.java
+++ src/tests/JobTest.java
@@ -10,7 +10,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import controller.JobController;
-import controller.ParkController;
 import model.Job;
 import model.Job.WorkDuty;
 import model.Park;

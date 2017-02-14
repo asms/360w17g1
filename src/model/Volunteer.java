80144047d89ec31c62a94094dbe0fa1e2d556c1d
ntrinh139 <1309.nguyentrinh@gmail.com>
2017-02-13 17:35:32 -0800

    small adjust

--- src/model/Volunteer.java
+++ src/model/Volunteer.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*
  * TCSS 360: Group 1
  * Assignment: Deliverable 2
@@ -80,4 +81,65 @@ public class Volunteer extends AbstractUser {
 				myEmailAddress
 		);
 	}
+=======
+package model;
+
+import java.util.Objects;
+
+public class Volunteer extends AbstractUser {
+
+	/**
+	 * Generated serial version UID.
+	 */
+	private static final long serialVersionUID = -1831128233221318812L;
+	
+	private final String myFirstName;
+	private final String myLastName;
+	private final String myPhoneNumber;
+	private final String myEmailAddress;
+	
+	/**
+	 * Constructs a volunteer.
+	 * @param theUserName the username
+	 * @param theFirstName the user's first name
+	 * @param theLastName the user's last name
+	 * @param thePhoneNumber the user's phone number
+	 * @param theEmailAddress the user's email address
+	 */
+	public Volunteer(final String theUserName, final String theFirstName, final String theLastName,
+			final String thePhoneNumber, final String theEmailAddress) {
+		super(theUserName);
+		myFirstName = Objects.requireNonNull(theFirstName);
+		myLastName = Objects.requireNonNull(theLastName);
+		myPhoneNumber = Objects.requireNonNull(thePhoneNumber);
+		myEmailAddress = Objects.requireNonNull(theEmailAddress);
+	}
+	
+	public String getFirstName() {
+		return myFirstName;
+	}
+	
+	public String getLastName() {
+		return myLastName;
+	}
+	
+	public String getPhoneNumber() {
+		return myPhoneNumber;
+	}
+	
+	public String getEmailAddress() {
+		return myEmailAddress;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s %s %s %s",
+				myFirstName,
+				myLastName,
+				myPhoneNumber,
+				myEmailAddress
+		);
+	}
+
+>>>>>>> Stashed changes
 }

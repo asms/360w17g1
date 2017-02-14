76a283f8498970749b515a63b4ebc54a4c696a78
ntrinh139 <1309.nguyentrinh@gmail.com>
2017-02-13 17:34:26 -0800

    ParkManager Test

--- src/tests/ParkManagerTest.java
+++ src/tests/ParkManagerTest.java
@@ -5,15 +5,20 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import model.ParkManager;
+import model.Job;
+import model.Park;
 
 /**
  * @author Steven Smith
  * @version 1.0
+ * <p>Getters are not tested because this is whitebox testing.</p>
  */
 public class ParkManagerTest {
 	
@@ -34,6 +39,51 @@ public class ParkManagerTest {
 		assertEquals(USERNAME, myParkManager.getUserName());
 	}
 	
-	//TODO: implement more test methods
-
+	/**
+	 * Test method for {@link ParkManager#associate(Park thePark)}
+	 */
+	@Test
+	public void testAssociate() {
+		HashMap<String, Park> expected = new HashMap<String, Park>();
+		expected = new ParkManager(USERNAME).getAssociatedParks();
+		HashMap<String, Park> actual = new HashMap<String, Park>();
+		actual = myParkManager.getAssociatedParks();
+		
+		assertEquals(expected, actual);	
+	}
+	
+	/**
+	 * Test method for {@link ParkManager#getAssociatedParks()}
+	 */
+	@Test
+	public void testGetAssociatedParks() {		
+		HashMap<String, Park> expected = new HashMap<String, Park>();
+		for (int i = 0; i < 100; i++) {
+			expected = new ParkManager(USERNAME).getAssociatedParks();
+		}
+		
+		HashMap<String, Park> actual = new HashMap<String, Park>();
+		for (int i = 0; i < 100; i++) {
+			actual = myParkManager.getAssociatedParks();
+		}
+			
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test method for {@link ParkManager#getJobs()}
+	 */
+	@Test
+	public void testGetJobs() {	
+		final ArrayList<Job> expectedJobs = new ArrayList<Job>();
+		for (int i = 0; i < 100; i++) {
+			expectedJobs.addAll(new ParkManager(USERNAME).getJobs());
+		}
+		
+		final ArrayList<Job> actualJobs = new ArrayList<Job>(); 
+			actualJobs.addAll(myParkManager.getJobs());
+			
+		assertEquals(expectedJobs, actualJobs);
+	}
+		
 }

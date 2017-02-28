7e5c714f003952c75c878c3bb76a350e6ff5f78a
ntrinh139 <1309.nguyentrinh@gmail.com>
2017-02-27 19:32:08 -0800

    ParkController Test

--- src/tests/ParkControllerTest.java
+++ src/tests/ParkControllerTest.java
@@ -18,78 +18,88 @@ import model.Park;
 
 /**
  * Test suite for {@link ParkController}.
- * 
  * @author Steven Smith
+ * @author Trinh Nguyen
  * @version 1.0
  */
 public class ParkControllerTest {
-	
-	/**
-	 * ParkController objects used for testing.
-	 */
-	private ParkController myParkController;
-
-	@Before
-	public void setup() {
-		myParkController = new ParkController();
-		myParkController.clear();
-	}
-	
-	@After
-	public void cleanup() {
-		myParkController.clear();
-	}
-	
-	/**
-	 * Test method for {@link ParkController#getParkByName(String)} 
-	 * Checks if a parks is retrievable by name.
-	 */
-	@Test
-	public void testGetParkByName() {
-		final Park expectedPark = new Park("Park Name 50", "Park Location 50");
-		myParkController.addPark(expectedPark);
-		final Park park = myParkController.getParkByName(expectedPark.getName());
-		assertEquals(expectedPark, park);
-	}
-	
-	/**
-	 * Test method for {@link ParkController#getParkByName(String)} 
-	 * Checks if a parks is retrievable by name after many have been added.
-	 */
-	@Test
-	public void testGetParkByNameFromMany() {
-		for (int i = 0; i < 100; i++) {
-			myParkController.addPark(new Park("Park Name " + i, "Park Location " + i));
-		}
-		
-		final Park expectedPark = new Park("Park Name 50", "Park Location 50");
-		final Park park = myParkController.getParkByName(expectedPark.getName());
-		assertEquals(expectedPark, park);
-	}
-	
-	/**
-	 * Test method for {@link ParkController#getAllParks(Park)} 
-	 * Checks if empty array is returned if no parks are available.
-	 */
-	@Test
-	public void testGetAllParksWhenEmpty() {
-		final ArrayList<Park> parks = myParkController.getAllParks();
-		assertTrue(parks.isEmpty()); // No exception is thrown and list is empty
-	}
-	
-	/**
-	 * Test method for {@link ParkController#getAllParks(Park)} 
-	 * Checks if all parks are added to and retrievable from the controller.
-	 */
-	@Test
-	public void testGetAllParks() {
-		final ArrayList<Park> parks = new ArrayList<Park>();
-		for (int i = 0; i < 100; i++) {
-			parks.add(new Park("Park Name " + i, "Park Location " + i));
-			myParkController.addPark(new Park("Park Name " + i, "Park Location " + i));
-		}
-		final ArrayList<Park> allParks = myParkController.getAllParks();
-		assertTrue(allParks.containsAll(parks) && parks.containsAll(allParks));
-	}
-
+    
+    /**
+     * ParkController objects used for testing.
+     */
+    private ParkController myParkController;
+    
+    /**
+     * Default park name to use for park test object.
+     */
+    private static final String PARK_NAME = "Park Name 50";
+    
+    /**
+     * Default park location to use for park test location.
+     */
+    private static final String PARK_LOCATION = "Park Location 50";
+    
+    @Before
+    public void setup() {
+        myParkController = new ParkController();
+        myParkController.clear();
+    }
+    
+    @After
+    public void cleanup() {
+        myParkController.clear();
+    }
+    
+    /**
+     * Test method for {@link ParkController#getParkByName(String)}
+     * Checks if a parks is retrievable by name.
+     */
+    @Test
+    public void testGetParkByName_SingleParkNameParkLocation_ExpectedTrue() {
+        final Park expectedPark = new Park(PARK_NAME, PARK_LOCATION);
+        myParkController.addPark(expectedPark);
+        final Park park = myParkController.getParkByName(expectedPark.getName());
+        assertEquals(expectedPark, park);
+    }
+    
+    /**
+     * Test method for {@link ParkController#getParkByName(String)}
+     * Checks if a parks is retrievable by name after many have been added.
+     */
+    @Test
+    public void testGetPark_ArrayOfParkNamesParkLocations_ExpectedTrue() {
+        for (int i = 0; i < 100; i++) {
+            myParkController.addPark(new Park("Park Name " + i, "Park Location " + i));
+        }
+        
+        final Park expectedPark = new Park(PARK_NAME, PARK_LOCATION);
+        final Park park = myParkController.getParkByName(expectedPark.getName());
+        assertEquals(expectedPark, park);
+    }
+    
+    /**
+     * Test method for {@link ParkController#getAllParks(Park)}
+     * Checks if empty array is returned if no parks are available.
+     */
+    @Test
+    public void testGetAllParks_EmptyArrayOfParkNameParkLocation_ExpectedTrue() {
+        final ArrayList<Park> parks = myParkController.getAllParks();
+        assertTrue(parks.isEmpty());
+    }
+    
+    /**
+     * Test method for {@link ParkController#getAllParks(Park)}
+     * Checks if all parks are added to and retrievable from the controller.
+     */
+    @Test
+    public void testGetAllParks_ArrayOfParkNamesAndParkLocations_ExpectedTrue() {
+        final ArrayList<Park> parks = new ArrayList<Park>();
+        for (int i = 0; i < 100; i++) {
+            parks.add(new Park("Park Name " + i, "Park Location " + i));
+            myParkController.addPark(new Park("Park Name " + i, "Park Location " + i));
+        }
+        final ArrayList<Park> allParks = myParkController.getAllParks();
+        assertTrue(allParks.containsAll(parks) && parks.containsAll(allParks));
+    }
+    
 }
